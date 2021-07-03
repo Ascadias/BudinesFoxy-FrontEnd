fc4774ce99c6647b8303012c4e6c554b92d18852
Marcelo Fernandez <goushart@gmail.com>
2021-07-03 04:55:11 -0300

    Changes in cover and favicon.ico

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './assets/images/Logo.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Budines Foxy</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Aplicación web en construcción, por favor regrese luego.</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
